0fed811b62363456f08c18d066d524b050bb313e
ALDOGH-PRO <aldo.dg469@gmail.com>
2025-01-27 10:47:09 -0600

    Create ALDOGH-PRO.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/ALDOGH-PRO.java
@@ -0,0 +1,19 @@
+//https://www.java.com/es/   //este es el URL de el lenguaje que me interesa aprrender
+
+//comentario de una linea     
+/*comentario de varias lineas*/
+/**cometario para documentacion*/
+
+string   Saludo= "hola buenos dias mi amor";//aqui declaro una variable 
+
+public static final double PI=3.1416;//aqui declaro una constante
+
+String sonrojado="ups que pena"
+  int califenamorado=10
+  boolean respuesta=false
+  bolean respuesta=true
+  float totalcuenta=100.000
+  double dinerototal=1.000,000,000,
+
+  System.out.println("¡Hola!, java);
+
